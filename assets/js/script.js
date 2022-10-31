cde9a7124b1547eda78de2bebfbe42734a2a6839
David Restivo <davidrestivo27@gmail.com>
2022-10-30 22:50:08 -0400

    fixed the JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,18 @@
-var TOD = moment().format('dddd, MMM Do YYYY');
-$("#currentTD").html(TOD);
-// Set the Time of Day on the page
+var date = moment().format('dddd, MMM Do YYYY');
+$("#currentDate").html(date);
+// Set the Date on the page
 
 $(document).ready(function () {
     $(".saveBtn").on("click", function () {
-      // Setting the inputs for each block after save is pressed     
+      // Setting the input text for each block after save is pressed     
         var enteredTxt = $(this).siblings(".description").val();
         var blockSelected = $(this).parent().attr("id");
        
         localStorage.setItem(blockSelected, enteredTxt);
     })
-   
-    function timeTracker() {
+  //  Checking the blocks and seeing if they are before the current timeTracker, currently the time or after the time and setting the appropriate class for each
+
+    function currTime() {
         var currentTIme = moment().hour();
 
         $(".time-block").each(function () {
@@ -21,31 +22,43 @@ $(document).ready(function () {
                 $(this).removeClass("future");
                 $(this).removeClass("present");
                 $(this).addClass("past");
+               
             }
-            else if (blockTime === currentTIme) {
+            else if (Time === currentTIme) {           
                 $(this).removeClass("past");
                 $(this).removeClass("future");
                 $(this).addClass("present");
+              
             }
             else {
                 $(this).removeClass("present");
                 $(this).removeClass("past");
                 $(this).addClass("future");
+             
 
             }
         })
     }
 
+    // recalling the entered itemed from local storage after a refresh of the page before  a clear of local storage is made
 
-    $("#hour9 .description").val(localStorage.getItem("9am"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#9am .description").val(localStorage.getItem("9am"));
+    $("#10am .description").val(localStorage.getItem("10am"));
+    $("#11am .description").val(localStorage.getItem("11am"));
+    $("#12pm .description").val(localStorage.getItem("12pm"));
+    $("#13pm .description").val(localStorage.getItem("13pm"));
+    $("#14pm .description").val(localStorage.getItem("14pm"));
+    $("#15pm .description").val(localStorage.getItem("15pm"));
+    $("#16pm .description").val(localStorage.getItem("16pm"));
+    $("#17pm .description").val(localStorage.getItem("17pm"));
 
-    timeTracker();
+    currTime();
 })
+
+// added a button to the page to clear the time blocks
+function clearLocal() {
+  localStorage.clear();
+
+  $(".description").val("");   
+
+}
